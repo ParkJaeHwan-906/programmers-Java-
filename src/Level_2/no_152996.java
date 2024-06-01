0251308ee30b4b788b2559322266bd269d964e9d
Hwannee00 <doormoo2@naver.com>
2024-06-01 13:39:09 +0900

    update

--- src/Level_2/no_152996.java
+++ src/Level_2/no_152996.java
@@ -36,19 +36,18 @@ public class no_152996 {
         long answer = 0;
         Arrays.sort(weights);
         Map<Double, Integer> map = new HashMap<>();
-        for(int i : weights){
+        for(int i : weights) {
             double a = (i * 1);
             double b = (i * 2) / 3;
             double c = (i * 1) / 2;
-            double d = (i* 3) / 4;
-            if(map.containsKey(a)) answer += map.get(a);
-            if(map.containsKey(b)) answer += map.get(b);
-            if(map.containsKey(c)) answer += map.get(c);
-            if(map.containsKey(d)) answer += map.get(d);
+            double d = (i * 3) / 4;
+            if (map.containsKey(a)) answer += map.get(a);
+            if (map.containsKey(b)) answer += map.get(b);
+            if (map.containsKey(c)) answer += map.get(c);
+            if (map.containsKey(d)) answer += map.get(d);
 
-            map.put(a, map.getOrDefault(a, 0)+1);
+            map.put(a, map.getOrDefault(a, 0) + 1);
         }
-        System.out.println(map);
         return answer;
 
     }
