9e54a64122c48c765eaa668c18027a52c526ec9d
Hwannee00 <doormoo2@naver.com>
2024-07-14 23:10:50 +0900

    start no_77485

---
+++ src/Level_2/no_77485.java
@@ -0,0 +1,21 @@
+package Level_2;
+
+// 프로그래머스 Lv.2
+// 행렬 테두리 회전하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/77485
+public class no_77485 {
+    public int[] solution(int rows, int columns, int[][] queries){
+        int [] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        int row = 6;
+        int columns = 6;
+        int[][] queries = {{2,2,5,4}, {3,3,6,6}, {5,1,6,3}};
+
+
+        // result : [8, 10, 25]
+    }
+}
