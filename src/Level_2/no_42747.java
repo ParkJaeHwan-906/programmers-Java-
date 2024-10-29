c89d0865da2485ef7f62feb613d3bd544cad33ea
Hwannee00 <doormoo2@naver.com>
2024-10-29 12:34:38 +0900

    pass 문제가 이해X

--- src/Level_2/no_42747.java
+++ src/Level_2/no_42747.java
@@ -1,9 +1,6 @@
 package Level_2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 // 프로그래머스 Lv.2
 // H-Index
@@ -15,38 +12,53 @@ public class no_42747 {
     ==> H-Index 이다.
      */
     public int solution(int[] citations){
-        int answer = 0;
+//        int answer = 0;
+//
+//        int n = citations.length;
+//
+//        // 내림차순으로 우선순위 큐
+//        PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> e2 - e1);
+//        for(int i : citations){
+//            pq.offer(i);
+//        }
+//
+//        while(!pq.isEmpty()){
+//            int h = pq.poll();
+//            System.out.println("h : "+h);
+//            int count = 0;
+//            for(int i : citations){
+//                // h 번 이상 인용되었다면
+//                if(i >= h){
+//                    count++;
+//                }
+//            }
+//            System.out.println("count : "+ count);
+//
+//            // n 편 중에 h 번 이상 인용된 논문이 h편 이상
+//            //    나머지 논문들이 h 번 이하로 인용
+//            if(h <= count && n-count <= h){
+//                answer = h;
+//                break;
+//            }
+//        }
+//
+//        return answer;
 
+        // citations 배열을 오름차순 정렬
+        Arrays.sort(citations);
         int n = citations.length;
 
-        // 내림차순으로 우선순위 큐
-        PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> e2 - e1);
-        for(int i : citations){
-            pq.offer(i);
-        }
-
-        while(!pq.isEmpty()){
-            int h = pq.poll();
-            System.out.println("h : "+h);
-            int count = 0;
-            for(int i : citations){
-                // h 번 이상 인용되었다면
-                if(i >= h){
-                    count++;
-                }
-            }
-            System.out.println("count : "+ count);
-
-            // n 편 중에 h 번 이상 인용된 논문이 h편 이상
-            //    나머지 논문들이 h 번 이하로 인용
-            if(h <= count && n-count <= h){
-                answer = h;
-                break;
+        // h-index 계산
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // 현재 h-index 후보
+            if (citations[i] >= h) {
+                return h; // h-index 조건을 만족하면 바로 반환
             }
         }
-
-        return answer;
+        return 0;
     }
+
+
 
     public static void main(String[] args){
         int[] citations = new int[] {5,6,7,8};
