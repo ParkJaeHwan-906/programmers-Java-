3a1cb7c0dde8e6cc61dbecadb5c09fdbd877041f
Hwannee00 <doormoo2@naver.com>
2024-11-02 00:39:00 +0900

    어렵다

--- src/Level_3/no_7726.java
+++ src/Level_3/no_7726.java
@@ -9,18 +9,24 @@ import java.util.*;
 // 나무 섭지
 // https://softeer.ai/practice/7726
 public class no_7726 {
-    static List<int[]> ghost = new ArrayList<>();
-    static int[] goal;
-    static int[] start;
+    static Queue<int[]> ghost = new LinkedList<>();
+    static Queue<int[]> human = new LinkedList<>();
+    static char[][] map;
+    static int n, m;
+    static int[][] ghostMap;
+    static int[][] humanMap;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // n,m 입력
         String[] nm = br.readLine().split(" ");
-        int n = Integer.parseInt(nm[0]);
-        int m = Integer.parseInt(nm[1]);
+        n = Integer.parseInt(nm[0]);
+        m = Integer.parseInt(nm[1]);
+
+        ghostMap = new int[n][m];
+        humanMap = new int[n][m];
 
         // map 입력
-        char[][] map = new char[n][m];
+        map = new char[n][m];
         for(int i=0; i<n; i++){
             // map 을 한줄씩 입력받음
             String s = br.readLine();
@@ -28,81 +34,83 @@ public class no_7726 {
                 map[i][j] = s.charAt(j);
                 // 유령, 목적지, 남수의 좌표를 기록
                 if(s.charAt(j) == 'G'){
-                    ghost.add(new int[] {i,j});
-                }
-                else if(s.charAt(j) == 'D'){
-                    goal = new int[]{i,j};
+                    ghost.offer(new int[] {i,j});
+                    ghostMap[i][j] = 1;
                 }
                 else if(s.charAt(j) == 'N'){
-                    start = new int[] {i,j};
+                    human.offer(new int[] {i,j});
+                    humanMap[i][j] = 1;
                 }
             }
         }
 
         no_7726 problem = new no_7726();
-        problem.solution(map);
+        boolean result = problem.solution(map);
+        System.out.println(result ? "Yes" : "No");
     }
 
-    int[][] ghostMap;
+
     public boolean solution(char[][] map){
         // 유령이 각 칸으로 갈 수 있는 시간 구하기
-        ghostMap = new int[map.length][map[0].length];
 
-        // 각 행을 최대 시간으로 채우기
-        for (int i = 0; i < ghostMap.length; i++) {
-            Arrays.fill(ghostMap[i], 987654321);
-        }
+        ghostMove();
 
-        for(int[] ghostPoint : ghost){
-            boolean[][] visited = new boolean[map.length][map[0].length];
-            ghostMove(ghostPoint[0], ghostPoint[1], visited);
-        }
+        boolean answer = humanMove();
 
-        for(int[] arr : ghostMap){
-            for(int i : arr){
-                System.out.print(i+" ");
-            }
-            System.out.println();
-        }
-
-        return true;
+        return answer;
     }
 
     int[] dx = new int[] {0, 1, 0, -1}; // x좌표 이동
     int[] dy = new int[] {1, 0, -1, 0}; // y좌표 이동
 
-    public void ghostMove(int startX, int startY, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startX, startY, 0});
+    // 유령의 동선 경로 계산
+    public void ghostMove(){
 
-        while (!q.isEmpty()) {
-            int[] node = q.poll();
-            ghostMap[node[0]][node[1]] = Math.min(ghostMap[node[0]][node[1]], node[2]);
-            visited[node[0]][node[1]] = true; // 방문 처리
+        while(!ghost.isEmpty()){
+            // 현재 위치
+            int[] node = ghost.poll();
+            // 방문 처리
+            if(map[node[0]][node[1]] == 'D') return;
 
-            for (int i = 0; i < 4; i++) {
-                int ny = node[0] + dy[i]; // y좌표 계산
-                int nx = node[1] + dx[i]; // x좌표 계산
+            // 상하좌우 이동
+            for(int i=0; i<4; i++){
+                int nx = node[0] + dx[i];
+                int ny = node[1] + dy[i];
 
-                if (nx >= 0 && nx < ghostMap[0].length && ny >= 0 && ny < ghostMap.length && !visited[ny][nx]) {
-                    q.offer(new int[]{ny, nx, node[2] + 1}); // 순서 주의
-                }
+                if(nx < 0 || ny < 0 || nx >= ghostMap.length || ny >= ghostMap[0].length) continue;
+                if(ghostMap[nx][ny] > 0) continue;  // 이미 방문한 적이 있음
+
+                ghost.offer(new int[]{nx, ny});
+                ghostMap[nx][ny] = ghostMap[node[0]][node[1]] + 1;
             }
         }
     }
+
+    public boolean humanMove() {
+        while(!human.isEmpty()){
+            int[] node = human.poll();
+            // 현재 위치 방문 처리
+            if(map[node[0]][node[1]] == 'D') return true;
+
+            for(int i=0; i<4; i++){
+                int nx = node[0] + dx[i];
+                int ny = node[1] + dy[i];
+
+                // map 영역을 벗어나지 않고, 방문하지 않았으며, 벽이 아닌곳
+                if(nx < 0 || ny < 0 || nx >= humanMap.length || ny >= humanMap[0].length) continue;
+                if(humanMap[nx][ny] > 0) continue;  // 이미 방문한 적이 있음
+                if(map[nx][ny] == '#') continue;
+                if(ghostMap[nx][ny] <= humanMap[node[0]][node[1]] + 1 && ghostMap[nx][ny] > 0) continue;
+
+                human.add(new int[] {nx, ny});
+                humanMap[nx][ny] = humanMap[node[0]][node[1]] + 1;
+            }
+        }
+        return false;
+    }
 }
 
-//    public class Node{
-//        int x;
-//        int y;
-//        int cnt;
-//
-//        public Node(int x, int y, int cnt){
-//            this.x = x;
-//            this.y = y;
-//            this.cnt = cnt;
-//        }
-//    }
+
 
 
 /*
