6802aaee681a521100597318a2f91bfc3e1951ee
Hwannee00 <doormoo2@naver.com>
2024-12-13 13:44:45 +0900

    출퇴근길 푸는중
    시간초과

--- src/Level_3/no_6248.java
+++ src/Level_3/no_6248.java
@@ -32,10 +32,87 @@ public class no_6248 {
             m--;
         }
 
+//        //  ✅ 도로 표현
+//        for(int i = 0; i < road.length; i++){
+//            System.out.println(i+1 + "연결");
+//            for(int j=0; j < road[i].size(); j++){
+//                System.out.print(road[i].get(j)+1+" ");
+//            }
+//            System.out.println();
+//        }
+
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken())-1;
         int end = Integer.parseInt(st.nextToken())-1;
 
+        no_6248 problem = new no_6248();
+        System.out.println(problem.solution(start, end));
+    }
 
+    Set<Integer> sToE = new HashSet<>();
+    Set<Integer> eToS = new HashSet<>();
+    public int solution(int s, int e) {
+        dfs(s, e, new boolean[road.length], 1);
+//        System.out.println();
+        dfs(e, s, new boolean[road.length], -1);
+
+//        System.out.println("출근길");
+//        for(int i : sToE) {
+//            System.out.print(i+" ");
+//        }
+//        System.out.println();
+//        System.out.println("퇴근길");
+//        for(int i : eToS) {
+//            System.out.print(i+" ");
+//        }
+
+//        System.out.println(eToS.contains(0));
+        return findDuplicated(s, e);
+    }
+
+    public void dfs(int s, int e, boolean[] visited, int flag) {
+        // 경로를 추가
+        if (flag == -1) {  // 퇴근
+            if(!eToS.contains(s)){
+                eToS.add(s);
+            }
+        } else {    // 출근
+            if(!sToE.contains(s)){
+                sToE.add(s);
+            }
+        }
+
+        if (s == e) { // 목적지에 도착한 경우
+            return;
+        }
+
+        visited[s] = true; // 방문 처리
+
+        for (int next : road[s]) {
+            if (visited[next]) continue; // 이미 방문한 경우 스킵
+
+            // 다음 노드가 유효한 경로로 연결되는 경우만 기록
+            dfs(next, e, visited, flag);
+
+        }
+
+        visited[s] = false; // 방문 해제 (백트래킹)
+    }
+
+    private int findDuplicated(int s, int e) {
+        int answer = 0;
+        if(sToE.size() > eToS.size()){
+            for(int i : sToE){
+                if(i == e || i == s || !eToS.contains(i)) continue;
+                answer++;
+            }
+        } else{
+            for(int i : eToS){
+                if(i == e || i == s || !sToE.contains(i)) continue;
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
