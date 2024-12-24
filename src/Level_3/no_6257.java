ec1a6f97830f2c7fd2ed153f394f8066290ebba7
Hwannee00 <doormoo2@naver.com>
2024-12-24 10:23:01 +0900

    통근버스 출발 순서 검증하기 해결중
    문제에 나온 조건 그대로 문제 이해하기

--- src/Level_3/no_6257.java
+++ src/Level_3/no_6257.java
@@ -23,71 +23,39 @@ public class no_6257 {
             bus[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(problem.solution());
+        System.out.println(problem.problemUnderStand());
     }
 
-    Queue<Integer> depart = new LinkedList<>();
-    public long solution() {
-        Stack<Integer> parkingLot = new Stack<>();
+    // 문제 이해하기
+    /*
+    (i, j, k) 가 있을 때
+    임의의 자연수 i < j < k 에 대하여 (1) a^i < a^j 이고 (2) a^i > a^k 인 경우가 있다면 정렬이 불가하다
+     */
+    public long problemUnderStand() {
+        long answer = 0;
 
-        for(int i : bus) {
-            System.out.println("현재 버스 : " + i);
-            System.out.println("주차장 : " + parkingLot);
-            System.out.println("출발 : " + depart);
-            while(!parkingLot.isEmpty()) {
-                System.out.println("비교 : " + parkingLot.peek());
-                if(parkingLot.peek() > i) break;
-                depart.offer(parkingLot.pop());
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int a = bus[i]; // a^i
+                int b = bus[j]; // a^j
+
+                if(a > b) continue;
+
+                // (1) 번 조건 만족
+                for(int k=j+1; k<n; k++){
+                    int c = bus[k];
+
+                    // (2) 번 조건 만족
+                    if(a>c) {
+                        // 정답 반영
+                        answer++;
+                    }
+
+                }
             }
-            parkingLot.push(i);
         }
 
-
-        System.out.println("마지막 주차장 : " + parkingLot);
-        while(!parkingLot.isEmpty()){
-            depart.offer(parkingLot.pop());
-        }
-        System.out.println("마지막 출발 : " + depart);
-        return isSeq();
+        return answer;
     }
 
-    public long isSeq(){
-        int pre = depart.poll();
-
-        while(!depart.isEmpty()) {
-            System.out.println("이전 : " + pre);
-
-            int now = depart.peek();
-
-            System.out.println("현재 : " + now);
-            System.out.println("차 : " + (now - pre));
-            if(now - pre != 1) break;
-            depart.poll();
-            pre = now;
-        }
-
-
-        System.out.println("chlchlchlchlchlchlwhd : "+depart);
-        return depart.isEmpty() ? 0 : depart.size() <= 3 ? 1 : combination();
-    }
-
-    public long combination(){
-        // depart 의 원소 중 순서쌍 (i,j,k) 3개를 뽑는 경우
-        int size = depart.size();
-        int peek = 3;
-
-        // size C peek
-        return factorial(size) / (factorial(peek) * factorial(size-peek));
-    }
-
-    public long factorial(int n) {
-        if(n == 0 || n == 1) return 1;
-
-        long result = 1;
-        for(int i = 2; i <= n; i++){
-            result *= i;
-        }
-
-        return result;
-    }
 }
