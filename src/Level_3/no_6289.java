d13b3681197f175c3355c60120caf374e4674a8e
Hwannee00 <doormoo2@naver.com>
2025-01-30 00:10:27 +0900

    우물안 개구리 해결

--- src/Level_3/no_6289.java
+++ src/Level_3/no_6289.java
@@ -11,8 +11,10 @@ public class no_6289 {
     static BufferedWriter bw;
     static int memberNum, relationNum;
     static int[] members;
-    static ArrayList[] relations;
+    static ArrayList<Integer>[] relations;
     public static void main(String[] args) throws IOException {
+        no_6289 problem = new no_6289();
+
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -22,7 +24,7 @@ public class no_6289 {
         relationNum = Integer.parseInt(st.nextToken()); // 관계의 수
         relations = new ArrayList[memberNum+1];
         for(int idx=0; idx <= memberNum; idx++) {
-            relations[idx] = new ArrayList<>();
+            relations[idx] = new ArrayList<Integer>();
         }
 
         st = new StringTokenizer(br.readLine().trim()); // 각 회원이 들 수 있는 무게
@@ -40,8 +42,11 @@ public class no_6289 {
             relations[memberA].add(memberB);
             relations[memberB].add(memberA);
         }
+        br.close();
 
-
+        bw.write(String.valueOf(problem.getImtheBest()));
+        bw.flush();
+        bw.close();
     }
 
     int answer;
@@ -50,6 +55,8 @@ public class no_6289 {
         answer = 0;
         checked = new boolean[memberNum+1];
 
+        compareStorng();
+
         return answer;
     }
 
@@ -57,8 +64,7 @@ public class no_6289 {
     /*
     현재 회원과 관계가 있는 회원을 찾는다
         본인이 더 크다면 체크한다
-        본인이 더 작다면
-            상대 회원의 관계를 확인한다
+        본인이 더 작다면 넘어간다
      */
     private void compareStorng() {
         for(int idx = 1; idx <= memberNum; idx++) {
@@ -69,6 +75,15 @@ public class no_6289 {
             }
 
             // 연결되어 있는 관계가 있음
+            // 모든 상대의 최대 무게를 확인하여 비교한다.
+            boolean imKing = true;
+            for(int member : relations[idx]) {
+                if(members[idx] <= members[member]) {    // 내가 들 수 있는 무게보다 상대가 들 수 있는 무게가 큰 경우
+                    imKing = false;
+                    break;
+                }
+            }
+            answer = imKing ?  answer + 1 : answer;
         }
     }
 }
