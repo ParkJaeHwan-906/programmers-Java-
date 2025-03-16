9ccf0338ee42f9704383640999626117202b6f57
Hwannee00 <doormoo2@naver.com>
2025-03-16 21:29:53 +0900

    백준 키 순서 해결

---
+++ src/Beakjoon/Graph/BOJ_2458_키순서_박재환.java
@@ -0,0 +1,97 @@
+package Beakjoon.Graph;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_2458_키순서_박재환 {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        init();
+
+        bw.flush();
+        bw.close();
+    }
+
+    static int nodes, edges;    // 노드의 개수, 간선의 개수
+    static List<Integer>[] graph;  // 학생 간의 연결 ( 정방항 )
+    static List<Integer>[] reverseGraph;  // 학생 간의 연결 ( 역방항 )
+    static int correctStudent;              // 순서가 정확한 학생
+    static void init() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        nodes = Integer.parseInt(st.nextToken());
+        edges = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[nodes+1];
+        reverseGraph = new ArrayList[nodes+1];
+        for(int student=0; student<=nodes; student++) {
+            graph[student] = new ArrayList<>();
+            reverseGraph[student] = new ArrayList<>();
+        }
+
+        for(int edge=0; edge<edges; edge++) {
+            st = new StringTokenizer(br.readLine().trim());
+
+            int tall = Integer.parseInt(st.nextToken());
+            int small = Integer.parseInt(st.nextToken());
+
+            graph[tall].add(small);
+            reverseGraph[small].add(tall);
+        }
+        br.close();
+
+        correctStudent = 0;
+        compareHeight();
+
+        bw.write(String.valueOf(correctStudent));
+    }
+
+    static boolean[] checkedStudents;
+    static void compareHeight() {
+        for(int student=1; student<=nodes; student++) {
+           checkedStudents = new boolean[nodes+1];
+           checkedStudents[student] = true;
+
+            searchCompare(student, false);
+            searchCompare(student, true);
+
+            // 모든 연결 관계를 확인한다.
+            // 모든 학생이 체크되어있다면, 이는 선후 관계를 확실하게 알 수 있다.
+            if(isCorrect()) correctStudent++;
+        }
+    }
+
+    static boolean isCorrect() {
+        for(int student=1; student <= nodes; student++) {
+            if(!checkedStudents[student]) return false;
+        }
+        return true;
+    }
+
+    /*
+        현재 학생과 연결되어 있는 관계를 모두 찾는다.
+        이때 정방향 역방향을 flag 값으로 두어 비교 대상을 다르게 구분한다.
+     */
+    static void searchCompare(int startStudent, boolean isReverse) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(startStudent);
+
+        while(!q.isEmpty()) {
+            int nowStudent = q.poll();
+
+            for(int connStudent : isReverse ? reverseGraph[nowStudent] : graph[nowStudent]) {
+                // 이미 확인 된 학생이라면 넘어간다.
+                if(checkedStudents[connStudent]) continue;
+
+                // 확인하지 않은 학생이라면
+                checkedStudents[connStudent] = true;
+                q.offer(connStudent);
+            }
+        }
+    }
+}
+
+
