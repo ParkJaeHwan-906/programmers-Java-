4e4a27741150a0f61f283349e5bea2d5797e0d31
Hwannee00 <doormoo2@naver.com>
2025-04-21 23:40:37 +0900

    소프티어 나무섭지 풀이

--- src/Softeer/Softeer_7726_나무섭지.java
+++ src/Softeer/Softeer_7726_나무섭지.java
@@ -52,26 +52,53 @@ public class Softeer_7726_나무섭지 {
 			
 			ghostTime = Math.min(ghostTime, dist);
 		}
-		 
-		
-		// 남우가 출구까지 최단 시간 
+		// 유령 최단 시간 확인
+//		System.out.println(ghostTime);
+
+		// 남우가 출구까지 최단 시간
+		minArrivedTime();
 	}
-	
+	/*
+		남우가 출구까지 이동하는 최단 시간을 구한다.
+	 */
 	static void minArrivedTime() {
+		int[] dx = {0,1,0,-1};
+		int[] dy = {1,0,-1,0};
+
 		Queue<int[]> q = new LinkedList<int[]>();
 		boolean[][] visited = new boolean[row][col];
 		
-		q.offer(new int[] {namX, namY});
+		q.offer(new int[] {namX, namY, 0});
 		visited[namX][namY] = true;
 		
 		while(!q.isEmpty()) {
 			int[] cur = q.poll();
 			int x = cur[0];
 			int y = cur[1];
-			
-			if(x == exitX && y == exitY) {	// 도착한 경우 
-				
+			int time = cur[2];
+
+			if(x == exitX && y == exitY) {	// 도착한 경우
+				System.out.println(ghostTime > time ? "Yes" : "No");
+				return;
+			}
+
+			// 최단 경로 탐색
+			for(int dir=0; dir<4; dir++) {
+				int nx = x + dx[dir];
+				int ny = y + dy[dir];
+
+				// 범위 확인
+				if(nx < 0 || ny < 0 || nx >= row || ny >= col) continue;
+				// 이전 방문 여부 확인
+				if(visited[nx][ny] || map[nx][ny] == '#') continue;
+
+				// 방문 가능
+				visited[nx][ny] = true;
+				q.offer(new int[] {nx, ny, time+1});
 			}
 		}
+
+		// 여기까지 나왔다는 것은 도달할 수 없다는 뜻
+		System.out.println("No");
 	}
 }
