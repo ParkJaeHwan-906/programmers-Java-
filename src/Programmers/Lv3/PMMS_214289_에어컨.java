31a3f2feec794d52bb904ce7b15227d1d3f03dcb
ParkJaeHwan <doormoo2@naver.com>
2025-05-27 14:15:44 +0900

    Programmers Lv3 에어컨 풀이 ( DP )

--- src/Programmers/Lv3/PMMS_214289_에어컨.java
+++ src/Programmers/Lv3/PMMS_214289_에어컨.java
@@ -1,64 +1,93 @@
 package Programmers.Lv3;
 
+import java.util.*;
+
 public class PMMS_214289_에어컨 {
     public static void main(String[] args) {
-        int temperature = 28;
-        int t1 = 18;
-        int t2 = 26;
-        int a = 10;
-        int b = 8;
-        int[] onboard = {0,0,1,1,1,1,1};
+        int temperature = -10;
+        int t1 = -5;
+        int t2 = 5;
+        int a = 5;
+        int b = 1;
+        int[] onboard = {0,0,0,0,0,1,0};
+//        int[] onboard = {0,0,0,0,0,0,0};
         System.out.println(new PMMS_214289_에어컨().solution(temperature, t1, t2, a, b, onboard));
     }
 
 
     int lowLimit, highLimit;        // 최저 희망 온도, 최고 희망 온도
     int aPrice, bPrice;             // 온도를 상/하 시키는데 소모되는 비용, 온도를 유지하는데 소모되는 비용
-    int minPrice;                   // 최소 비용
     public int solution(int temperature, int t1, int t2, int a, int b, int[] onboard) {
-        lowLimit = t1; highLimit = t2;
+        lowLimit = t1+10; highLimit = t2+10;		// 온도의 범위가 -10 ~ 40 -> 0 ~ 50 으로 변환
         aPrice = a; bPrice = b;
-        minPrice = Integer.MAX_VALUE;
 
-        // 희망온도 구간의 각 온도별로, 유지 비용을 계산한다.
-        for(int targetTemp=lowLimit; targetTemp<highLimit+1; targetTemp++) {
-            // 매 케이스마다, 현 온도를 외부 기온으로 맞춘다.
-            calcPrice(0, 0, 0, Math.abs(temperature-targetTemp), onboard);
-        }
-
-        return minPrice;
+        return getMinCost(temperature+10, onboard);
     }
 
     /*
-        나올 수 있는 케이스,
-        1. 희망온도까지 올린 다음, 유지하기
-        2. 손님이 아예 타지 않는 경우 희망온도까지 올릴 필요가 없음
-
-        [!! 영하의 온도를 가질 수 있음 !!]
-        조합?
-        1. 에어컨을 가동한다.
-        2. 에어컨을 가동하지 않는다.
+        DP[][] => [현재시간][현재온도] = 최소 비용
+        temperature : 외부 기온 
+        onboard		: 승객 탑승 여부
      */
-    void calcPrice(int guestIdx, int nowTemp, int sum, int targetTemp, int[] onboard) {
-        if(guestIdx == onboard.length) {    // 모든 승객을 다 태운 경우
-            minPrice = Math.min(sum, minPrice);
-            return;
-        }
-        // 승객을 더 태울 수 있다.
+    int getMinCost(int temperature, int[] onboard) {
+    	int[][] dp = new int[onboard.length][51];
+    	for(int[] arr : dp) {
+    		Arrays.fill(arr, 100*1000+1);
+    	}
+    	
+    	// 초기값 설정
+    	// 0 분에 외부온도 비용은 0
+    	dp[0][temperature]=0; 
+    	
+    	for(int time=0; time<onboard.length-1; time++) {
+    		/*
+    		 * 에어컨을 킨다
+    		 * - 온도 증가
+    		 * - 온도 감소
+    		 * - 온도 유지
+    		 * 
+    		 * 에어컨을 끈다
+    		 * - 온도 증가
+    		 * - 온도 감소
+    		 */
+    		for(int temp=0; temp<51; temp++) {
+    			// 손님이 타있지만, 희망온도 범주 내에 있지 않는 경우 
+    			if(onboard[time] == 1 && (lowLimit > temp || highLimit < temp)) continue;
+    			
+    			// 에어컨을 킨다.
+    			// - 온도를 증가시킨다.
+    			if(temp < 50) dp[time+1][temp+1] = Math.min(dp[time+1][temp+1], dp[time][temp]+aPrice);
+    			// - 온도를 감소시킨다.
+    			if(temp > 0) dp[time+1][temp-1] = Math.min(dp[time+1][temp-1], dp[time][temp]+aPrice);
+    			// -현 온도 유지
+    			dp[time+1][temp] = Math.min(dp[time+1][temp], dp[time][temp]+bPrice);
+    			
+    			// 에어컨을 끈다.
+    			// + 현재 온도와 외부 기온이 같다면
+    			if(temp == temperature) dp[time+1][temp] = Math.min(dp[time+1][temp], dp[time][temp]);
+    			// - 온도를 증가시킨다.
+    			else if(temp < 50 && temp < temperature) dp[time+1][temp+1] = Math.min(dp[time+1][temp+1], dp[time][temp]);
+    			// - 온도를 감소시킨다.
+    			else if(temp > 0 && temp > temperature) dp[time+1][temp-1] = Math.min(dp[time+1][temp-1], dp[time][temp]);
+    		}
+    	}
+    	
+    	// 마지막 시간에 최대값을 반환
+    	return findMin(dp[onboard.length-1], onboard);
+    }
+    
+    int findMin(int[] dp, int[] onboard) {
+    	int min = Integer.MAX_VALUE;
 
-        // 1. 승객을 태워야하는데, 희망온도를 맞추지 못하는 경우
-        // 1-1. 이전의 최소 금액을 이미 넘긴 경우
-        if((onboard[guestIdx] == 1 && (lowLimit > nowTemp && highLimit < nowTemp)) || sum >= minPrice) return;
-
-        // 2. 에어컨을 켠다.
-        if(nowTemp == targetTemp) { // 현재 온도와 같은 경우
-            calcPrice(guestIdx+1, nowTemp, sum+bPrice, targetTemp, onboard);
-        } else {
-            calcPrice(guestIdx+1, nowTemp+1, sum+aPrice, targetTemp, onboard);
-        }
-
-        // 3. 에어컨을 끈다.
-        calcPrice(guestIdx+1, nowTemp-1, sum, targetTemp, onboard);
+    	/*
+    	 * 마지막 시간에 손님이 탈 수도, 타지 않을 수도 있음
+    	 */
+    	for(int temp=0; temp<51; temp++) {
+    		// 손님이 타게된다면, 희망 온도 내에 있어야함
+    		if(onboard[onboard.length-1] == 1 && (lowLimit > temp || highLimit < temp)) continue;
+    		min = Math.min(min, dp[temp]);
+    	}
+    	return min;
     }
 }
 
@@ -75,4 +104,14 @@ public class PMMS_214289_에어컨 {
 
     에어컨 소비전력은 희망온도와 실내 온도가 다르다면 1분에 a 만큼 소모,
     같다면 b 만큼 소모한다. 
+    
+  [시도 1]  
+     나올 수 있는 케이스,
+        1. 희망온도까지 올린 다음, 유지하기
+        2. 손님이 아예 타지 않는 경우 희망온도까지 올릴 필요가 없음
+
+        [!! 영하의 온도를 가질 수 있음 !!]
+        조합? XX
+        1. 에어컨을 가동한다.
+        2. 에어컨을 가동하지 않는다.
  */
