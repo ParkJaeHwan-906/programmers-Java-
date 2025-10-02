9cac8b19ecb22250db1ca3e5b08785fb42a2224e
ParkJaeHwan <doormoo2@naver.com>
2025-10-02 17:03:55 +0900

    여왕개미 풀이중

---
+++ src/CodeTree/여왕개미.java
@@ -0,0 +1,18 @@
+package CodeTree;
+
+import java.util.*;
+import java.io.*;
+
+public class 여왕개미 {
+    static BufferedReader br;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        init();
+        br.close();
+    }
+    static void init() throws IOException {
+
+    }
+}
